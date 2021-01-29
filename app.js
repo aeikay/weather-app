736ae405236e1ac98b29cc21557ee0f32e4f3150
aeikay <anandakrishnanayyappan@gmail.com>
2021-01-29 12:31:06 +0530

    added forecast

--- app.js
+++ app.js
@@ -5,10 +5,43 @@ const date = document.querySelector('.date')
 const element = document.body;
 const button = document.getElementById('darkmode');
 
+const table = document.querySelector('.table');
+
+const forecast1 = document.querySelector('.forecast1');
+const forecast2 = document.querySelector('.forecast2');
+const forecast3 = document.querySelector('.forecast3');
+const temp1 = document.querySelector('.temp1');
+const temp2 = document.querySelector('.temp2');
+const temp3 = document.querySelector('.temp3');
+
+const tempE1 = document.querySelector('.tempE1')
+const tempE2= document.querySelector('.tempE2')
+const tempE3 = document.querySelector('.tempE3')
+
+const tempN1 = document.querySelector('.tempN1');
+const tempN2 = document.querySelector('.tempN2');
+const tempN3 = document.querySelector('.tempN3');
+
+const desc1 = document.querySelector('.desc1');
+const desc2 = document.querySelector('.desc2');
+const desc3 = document.querySelector('.desc3');
+
+const forecastbutton = document.getElementById('forecastbutton')
+
+
+let forecast = [forecast1,forecast2,forecast3];
+let temperature = [temp1,temp2,temp3];
+let temperatureE = [tempE1,tempE2,tempE3];
+let temperatureN = [tempN1,tempN2,tempN3];
+let descriptionW = [desc1,desc2,desc3];
+
 let dat = new Date();
-let hours = dat.getHours();
+let hours = dat.getDate();
 let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 date.textContent = dat.toLocaleString('en-GB', options);
+
+
+
 
 button.addEventListener('click',()=>{
     element.classList.toggle("dark");
@@ -25,7 +58,8 @@ button.addEventListener('click',()=>{
 })
 
 window.addEventListener('load',()=>{
-
+    
+    table.style.display = "none";
     let long;
     let lat;
     if(navigator.geolocation){
@@ -33,27 +67,66 @@ window.addEventListener('load',()=>{
             // console.log(data.coords);
             long = data.coords.longitude;
             lat  = data.coords.latitude;
-            let api  = `https://api.openweathermap.org/data/2.5/weather?lat=`+lat+ `&lon=` + long + `&appid=` + `56d7df6c2080d7a0c7e9b8a36a901dfc`;
+            let api  = `https://api.openweathermap.org/data/2.5/onecall?lat=`+lat+ `&lon=` + long + `&exclude=minutely,hourly&appid=56d7df6c2080d7a0c7e9b8a36a901dfc`;
 
     fetch(api).then((response)=>{
         return response.json()
     }).then((data)=>{
-        console.log(data);
-            city.textContent = data.name;
-            temp.textContent = data.main.temp +' K';
-            let tempC = data.main.temp-273.15;
+        console.log(data.current);
+            city.textContent = data.timezone;
+            temp.textContent = data.current.temp +' K';
+            let tempC = data.current.temp-273.15;
             let tempinC = Math.round(tempC*10)/10;
 
+            for(let i=1;i<4;i++){     
+            let nd = new Date(data.daily[i].dt * 1000);
+            forecast[i-1].textContent = nd.toLocaleString('en-GB',options);
+
+            let tempC = data.daily[i].temp.day-273.15;
+            let tempinC = Math.round(tempC*10)/10;
+            temperature[i-1].textContent = tempinC;
+
+            let tempEC = data.daily[i].temp.eve - 273.15;
+            let tempECinC = Math.round(tempEC*10)/10;
+            temperatureE[i-1].textContent = tempECinC;
+
+            let tempNC = data.daily[i].temp.night - 273.15;
+            let tempNCinC = Math.round(tempNC*10)/10;
+            temperatureN[i-1].textContent = tempNCinC;
+
+            descriptionW[i-1].textContent = data.daily[i].weather[0].description;
+
+
+            }
             temp.addEventListener('click',()=>{
                 let str = temp.textContent;                
                 if(str.charAt(str.length-1) === 'C' ){
-                    temp.textContent = data.main.temp + ' K' ;
+                    temp.textContent = data.current.temp + ' K' ;
                 }else{
                     temp.textContent = tempinC + 'C';
                 }
             })
-            desc.textContent = data.weather[0].description;
-        });        
+            desc.textContent = data.current.weather[0].description;
+        }); 
+        
+        forecastbutton.addEventListener('click',()=>{
+            if(table.style.display=="none"){
+                   table.style.display = "table";
+                }
+            else{
+               console.log(table.style.display);
+            
+               table.style.display = "none";
+           }
+        })
+        
+
+        //tooltip
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();   
+});
+        // let api2 = `https://api.openweathermap.org/data/2.5/onecall?lat=` + lat + `&lon=`+ long + `&exclude=minutely,hourly&appid=56d7df6c2080d7a0c7e9b8a36a901dfc`
     }
     )
 }
